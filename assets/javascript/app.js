b8ce73295790c5902f9699a115f60dc397ea97d8
Richelle Burke <richelleburke@Richelles-MacBook-Pro.local>
2018-11-17 16:38:45 -0500

    v1: built basic html and css w/ bootstrap, collected audio files, built first object in app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,81 @@
+
+// 1) Hairspay
+// 2) The Fantasticks
+// 3) Aida
+// 4) Come From Away 
+// 5) Into the Woods
+// 6) Next to Normal 
+// 7) Once on this Island
+// 8) Pippin
+// 9) A Chorus Line
+// 10) Dear Evan Hansen 
+
+var trivia = {
+    
+    questions: [
+
+        hairspray = {
+            name: "Hairspray",
+            opSong: "goodmorningbalitmore.mp3",
+            choices: ["Once on this Island","Hairspray","Dear Evan Hansen","Aida"]
+        },
+
+        theFantasticks = {
+            name: "The Fantasticks",
+            opSong: "trytoremember.mp3",
+            choices: ["The Fantasticks","Come From Away","Pippin","Next to Normal"]
+
+        },
+
+        aida = {
+            name: "Aida",
+            opSong: "everystoryisalovestory.mp3",
+            choices: ["Aida","A Chorus Line","Into the Woods","Pippin"]
+
+        },
+
+        comeFromAway = {
+            name: "Come From Away",
+            opSong: "welcometotherock.mp3",
+            choices: ["Hairspray","Dear Evan Hansen","Once on this Island","Come From Away"]
+        },
+
+        intoTheWoods = {
+            name: "Into the Woods",
+            opSong: "prologue.mp3",
+            choices: ["A Chorus Line","The Fantasticks","Into the Woods","Aida"]
+        },
+
+        nextToNormal = {
+            name: "Next to Normal",
+            opSong: "justanotherday.mp3",
+            choices: ["Next to Normal","Pippin","Dear Evan Hansen","Hairspray"]
+        },
+
+        onceOnThisIsland = {
+            name: "Once on this Island",
+            opSong: "wedance.mp3",
+            choices: ["Aida","Next to Normal","Once on this Island","Into the Woods"]
+        },
+
+        pippin = {
+            name: "Pippin",
+            opSong: "magictodo.mp3",
+            choices: ["Come From Away","A Chorus Line","The Fantasticks","Pippin"]
+        },
+
+        aChorusLine = {
+            name: "A Chorus Line",
+            opSong: "ihopeigetit.mp3",
+            choices: ["Once on this Island","Hairspray","A Chorus Line","Dear Evan Hansen"]
+        },
+
+        dearEvanHansen = {
+            name: "Dear Evan Hansen",
+            opSong: "anybodyhaveamap.mp3",
+            choices: ["Into the Woods","Come From Away","Dear Evan Hansen","Next to Normal"]
+        }
+    ]
+}
+
+
